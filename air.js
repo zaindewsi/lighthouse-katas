f9da7fddadcfbf4fd8b43390025501d5fc889254
Zain Dewsi <zain.dewsi@gmail.com>
2021-04-03 15:52:14 +0000

    air quality challenge

---
+++ air.js
@@ -0,0 +1,32 @@
+const checkAir = function (samples, threshold) {
+  let clean = 0;
+  let dirty = 0;
+  
+  for (let i = 0; i < samples.length; i++) {
+    if (samples[i] === "clean") {
+      clean ++
+    } else {
+      dirty ++
+    }
+  }
+  if (dirty / (clean+dirty) > threshold) {
+    return "Polluted"
+  } else {
+    return "Clean"
+  }
+};
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
