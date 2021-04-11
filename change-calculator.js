67681840b6b7a574b267abc79b5831caf2f9cd4a
Zain Dewsi <zain.dewsi@gmail.com>
2021-04-11 14:46:06 +0000

    more katas

---
+++ change-calculator.js
@@ -0,0 +1,58 @@
+const calculateChange = (total, cash) => {
+  let change = cash - total
+  const denominations = {
+    twentyDollar: 0,
+    tenDollar: 0,
+    fiveDollar: 0,
+    twoDollar: 0,
+    oneDollar: 0,
+    quarter: 0,
+    dime: 0,
+    nickle: 0,
+    penny: 0
+  }
+  while (change > 0) {
+    if(change >= 2000) {
+      denominations.twentyDollar += 1;
+      change -= 2000
+    } else if(change >= 1000) {
+      denominations.tenDollar += 1;
+      change -= 1000
+    } else if(change >= 500) {
+      denominations.fiveDollar += 1;
+      change -= 500
+    } else if(change >= 200) {
+      denominations.twoDollar += 1;
+      change -= 200
+    } else if(change >= 100) {
+      denominations.oneDollar += 1;
+      change -= 100
+    } else if(change >= 25) {
+      denominations.quarter += 1;
+      change -= 25
+    } else if(change >= 10) {
+      denominations.dime += 1;
+      change -= 10
+    } else if(change >= 5) {
+      denominations.nickle += 1;
+      change -= 5
+    } else if(change >= 1) {
+      denominations.penny += 1;
+      change -= 1
+    }
+  }
+
+  for(key in denominations) {
+    if(denominations.hasOwnProperty(key)) {
+      var value = denominations[key]
+      if (value === 0) {
+      delete denominations[key]
+    }
+    }
+  }
+  return denominations
+}
+
+console.log(calculateChange(1787, 2000));
+console.log(calculateChange(2623, 4000));
+console.log(calculateChange(501, 1000));
---
+++ talking-calendar.js
@@ -0,0 +1,69 @@
+const talkingCalendar = date => {
+  date = date.split("/");
+  let year = date[0]
+  let month = date[1]
+  let day = date[2]
+
+  switch (month) {
+    case "01":
+      month = "January"
+      break;
+    case "02":
+      month = "February"
+      break;
+    case "03":
+      month = "March"
+      break;
+    case "04":
+      month = "April"
+      break;
+    case "05":
+      month = "May"
+      break;
+    case "06":
+      month = "June"
+      break;
+    case "07":
+      month = "July"
+      break;
+    case "08":
+      month = "August"
+      break;
+    case "09":
+      month = "September"
+      break;
+    case "10":
+      month = "October"
+      break;
+    case "11":
+      month = "November"
+      break;
+    case "12":
+      month = "December"
+      break;
+
+    default:
+      break;
+  }
+
+  if (date[2][0] === "0"){
+    day = date[2][1];
+  }
+  if (day === "1") {
+    day += "st"
+  } else if (day === "2") {
+    day += "nd"
+  } else if (day === "3") {
+    day += "3rd"
+  } else {
+    day += "th"
+  }
+
+
+  return month + " " + day + ", " + year
+  
+};
+
+console.log(talkingCalendar("2017/12/02"));
+console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("1987/08/24"));
